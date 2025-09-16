59bcb06187545277d3822c21a0197ee4ce4a4302
Cristian <cbolanosmar@gmail.com>
2025-09-16 10:17:52 -0500

    un nuevo archivo

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const routerApi = require('./routes');
+const cors = require('cors');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 app.use(express.json());
+const whitelist = ['http://127.0.0.1:5500', 'https://myapp.com'];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.send('Hello mi server en express');
